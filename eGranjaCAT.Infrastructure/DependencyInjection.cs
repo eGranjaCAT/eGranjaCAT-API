edc72796df0c72bae832c4b7cad9c7d111db6411
felixmk0 <fmontrakruse@gmail.com>
2025-08-29 12:16:50 +0200

    small fix

--- eGranjaCAT.Infrastructure/DependencyInjection.cs
+++ eGranjaCAT.Infrastructure/DependencyInjection.cs
@@ -16,7 +16,7 @@ namespace eGranjaCAT.Application
         public static IServiceCollection AddInfrastructureDI(this IServiceCollection services, IConfiguration configuration)
         {
 
-            services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(configuration.GetConnectionString("PostgresConnection")));
+            services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(configuration["PostgresConnection"]));
 
             services.AddIdentityCore<User>(options =>
             {
@@ -51,7 +51,7 @@ namespace eGranjaCAT.Application
             using (var scope = serviceProvider.CreateScope())
             {
                 var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-                db.Database.Migrate(); 
+                db.Database.Migrate();
             }
 
             return services;
